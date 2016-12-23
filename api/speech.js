49afe36b53a838f488ab2e37b2194d350b328bed
Anıl Güleroğlu <anil_guleroglu@hotmail.com>
2016-12-24 02:23:05 +0300

    development

--- api/speech.js
+++ api/speech.js
@@ -13,22 +13,30 @@ const client = new Wit({accessToken: 'PV5JC3Y42MSMBSTPWHGZ3P4UYQMRM66C'});
 client.message(req.query.q, {})
 .then((data) => {
   try {
+    console.log(JSON.stringify(data));
     var intent = data.entities.intent!=undefined?data.entities.intent[0].value:null;
     var category = data.entities.article_category!=undefined?data.entities.article_category[0].value:null;
     var type = data.entities.type!=undefined?data.entities.type[0].value:null;
+    var count = data.entities.article_count!=undefined?data.entities.article_count[0].value:null;
 
     var speechData = {
        Intent:intent,
        Category:category,
-       Type : type
+       Type : type,
+       Count : count==null?10:count
     }
-   
-        hurriyet.getArticles(10,['Description'],speechData.Category,function(result){
+
+   if(speechData.Intent=='read'){
+        hurriyet.getArticles(speechData.Count,['Description'],speechData.Category,function(result){
 
             speechData.data = result;
             res.json(speechData);
 
         });
+    }
+    else{
+        res.json(speechData);
+    }
    
 
 } catch (err) {
