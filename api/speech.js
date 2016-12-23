d4d194e909f99df1f35c659ae31c0b956a8607ed
Anıl Güleroğlu <anil_guleroglu@hotmail.com>
2016-12-24 01:10:32 +0300

    speech api added

--- api/speech.js
+++ api/speech.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var http = require('http');
-
+var hurriyet = require('../modules/hurriyet.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,28 +13,30 @@ const client = new Wit({accessToken: 'PV5JC3Y42MSMBSTPWHGZ3P4UYQMRM66C'});
 client.message(req.query.q, {})
 .then((data) => {
   try {
-  var intent = data.entities.intent!=undefined?data.entities.intent[0].value:null;
-   var category = data.entities.article_category!=undefined?data.entities.article_category[0].value:null;
-   var type = data.entities.type!=undefined?data.entities.type[0].value:null;
+    var intent = data.entities.intent!=undefined?data.entities.intent[0].value:null;
+    var category = data.entities.article_category!=undefined?data.entities.article_category[0].value:null;
+    var type = data.entities.type!=undefined?data.entities.type[0].value:null;
 
-   var speechData = {
-
+    var speechData = {
        Intent:intent,
        Category:category,
        Type : type
-
-   }
-    console.log('SUCCESS');
+    }
    
-res.send(JSON.stringify(speechData));
+        hurriyet.getArticles(10,['Description'],speechData.Category,function(result){
+
+            speechData.data = result;
+            res.json(speechData);
+
+        });
+   
 
 } catch (err) {
     console.log('ERR');
     console.log(err);
-  res.send(JSON.stringify(err));
+  res.json(err);
 }
    
-
 
 })
 
--- modules/hurriyet.js
+++ modules/hurriyet.js
@@ -1,9 +1,23 @@
 var http = require('https');
-
+var odata = require('odata-client');
 
 var hurriyet = function(){
 
     var self = {};
+    self.enChars = function(txt){
+        if(txt==null){
+            return null;
+        }
+        txt = txt.toLowerCase();
+        txt = txt.replace('ü','u');
+        txt = txt.replace('ı','i');
+        txt = txt.replace('ç','c');
+        txt = txt.replace('ş','s');
+        txt = txt.replace('ğ','g');
+
+        return txt;
+
+    } 
  //"path": "/v1/"+apiUrl+"?%24top="+l+"&%24select="+select,
 self.options = function(apiUrl,articleId,top,select,filter){
  var options = {
@@ -20,20 +34,38 @@ return options;
 }
 
 self.getArticles = function(top,select,filter,callback){
-    var opt = self.options("articles","","?%24top="+top,"&%24select="+select+"&%24filter="+filter+"");
-        var request = http.request(opt,function(res){
-            var datas =  [];
-            res.on("data",function(data){
-                datas.push(data);
-            });
 
-            res.on("end",function(){
-                var data = Buffer.concat(datas);
-                //console.log(data.toString());
-                callback(data.toString());
-            });
-        });
-        request.end();
+var q = odata({service: 'https://api.hurriyet.com.tr/v1/', resources: 'articles',headers:{apikey:'e7de90624f1c4d01b404ba44b2d2d865'}});
+q.top(top).select('Id').select('Title');
+
+for(var s in select){
+    q.select(select[s]);
+}
+if(filter!=null){
+q = q.filter("Path eq '/"+self.enChars(filter)+"/'");
+}
+q.get().then(function(response){
+
+    callback(response.body);
+
+});
+
+    // var f = filter!=null?("&%24filter=PATH%20eq%20'/"+filter+"/"):"";
+    // var opt = self.options("articles","","?%24top="+top,"&%24select="+select,f);
+    // console.log(opt);
+    //     var request = http.request(opt,function(res){
+    //         var datas =  [];
+    //         res.on("data",function(data){
+    //             datas.push(data);
+    //         });
+
+    //         res.on("end",function(){
+    //             var data = Buffer.concat(datas);
+    //             //console.log(data.toString());
+    //             callback(JSON.parse(data.toString()));
+    //         });
+    //     });
+    //     request.end();
     }
 
     self.getSingleArticle = function(Id,select,callback)
