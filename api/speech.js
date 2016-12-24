ef452d263a5a859aa43128225dc283116d3bf06b
Anıl Güleroğlu <anil_guleroglu@hotmail.com>
2016-12-24 11:45:52 +0300

    speech to mongo

--- api/speech.js
+++ api/speech.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+
 var Article = require('../models/Article.js'); 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+ Article.find().exec(function(err,dic){
+                console.log('else '+dic);
+            });
 
 const {Wit, log} = require('node-wit');
 const client = new Wit({accessToken: 'PV5JC3Y42MSMBSTPWHGZ3P4UYQMRM66C'});
@@ -22,8 +28,8 @@ client.message(req.query.q, {})
        Count : count==null?3:count,
        Data:null
     }
-
-   if(speechData.Intent=='read'){
+    if(req.query.type=='first-interaction'){
+        if(speechData.Intent=='read'){
         if(speechData.Category!=null){
             Article.find({Category:speechData.Category}).limit(speechData.Count).then(function(dic){
                 console.log(dic);
@@ -32,16 +38,39 @@ client.message(req.query.q, {})
 
             });
         }else{ 
-            Article.find({}).exec(function(err,dic){
+            Article.find({}).limit(speechData.Count).exec(function(err,dic){
                 console.log('else '+dic);
                 speechData.Data = dic;
                 res.json(speechData); 
             });
         }
-        
     }
-    else{
+    }
+    else if(req.query.type=='after-read'){
         res.json(speechData);
+    }
+    else if(req.query.type=='endof-read-interaction'){
+        var skip = req.query.skip;
+        if(speechData.Intent=='read' || speechData.Intent=='continue'){
+            //Skip ve limite göre data dönmem lazım. Kategori
+            if(speechData.Category!=null){
+            Article.find({Category:speechData.Category}).skip(skip).limit(speechData.Count).then(function(dic){
+                console.log(dic);
+                speechData.Data = dic;
+                res.json(speechData);
+
+            });
+        }else{ 
+            Article.find({}).skip(skip).limit(speechData.Count).exec(function(err,dic){
+                console.log('else '+dic);
+                speechData.Data = dic;
+                res.json(speechData); 
+            });
+        }
+        }
+        else if(speechData.Intent=='no' || speechData.Intent=='stop'){
+            res.json(speechData);
+        }
     }
 
 });
