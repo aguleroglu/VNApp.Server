b49439254334d84c08b3508605654d6e174f2b1f
Anıl Güleroğlu <anil_guleroglu@hotmail.com>
2016-12-24 08:58:33 +0300

    sign in sign up

--- api/user.js
+++ api/user.js
@@ -9,6 +9,22 @@ var crypto = require('crypto');
 
 const secret = 'vn-app-secret';
 
+router.post('/signin',function(req,res,next){
+
+var pwd = crypto.createHmac('sha256', secret)
+                        .update(req.body.password)
+                        .digest('hex');
+
+User.findOne({Email:req.body.email,Password:pwd}).exec(function(err,response){
+    if(response!=null){
+        
+                res.json({success:true,id:response.id});
+        }
+        else{
+            res.json({success:false,message:'Email or Password incorrect'});
+        }
+    });
+});
 
 router.post('/signup',function(req,res, next){
 
